4b8abc1c0e2a0012843085b01e16a2d3c85f0315
god su <sucunzhu@kuaishou.com>
2024-12-08 18:24:45 +0800

    xiughai9

--- main.cpp
+++ main.cpp
@@ -7,46 +7,57 @@
 #include <fstream>
 using namespace std;
 
-struct Person {
-    std::string name;
-    int age;
-};
+// struct Person {
+//     std::string name;
+//     int age;
+// };
 
-template<>
-class MyParse<Person> {
-public:
-    static Person parse(std::string str) {
-        Person p;
-        std::string::size_type pos = str.find(',');
-        if (pos == std::string::npos) {
-            throw std::invalid_argument("can not find ',' for st");
-        }
-        p.name =  str.substr(0, pos);
-        p.age = stoi(str.substr(pos+1, str.size()-pos-1));
-        return p;
-    }
-};
+// class MyParse<Person> {
+// public:
+//     static Person parse(std::string str) {
+//         Person p;
+//         std::string::size_type pos = str.find(',');
+//         if (pos == std::string::npos) {
+//             throw std::invalid_argument("can not find ',' for st");
+//         }
+//         p.name =  str.substr(0, pos);
+//         p.age = stoi(str.substr(pos+1, str.size()-pos-1));
+//         return p;
+//     }
+// };
 
 
 
 int main() {
     LineParser lp;
-    Parser<vector<int> > *parser = new Parser<vector<int> >();
-    Parser<Person > *parserPerson = new Parser<Person>();
-    Parser<string > *parserString = new Parser<string >();
-    Parser<float > *parserFloat = new Parser<float >();
-    Parser<vector<float> > *parserFloatVector = new Parser<vector<float> >();
-    lp.addParse(parser);
-    lp.addParse(parserPerson);
-    lp.addParse(parserString);
-    lp.addParse(parserFloat);
-    lp.addParse(parserFloatVector);
-    lp.parse("3:1,2,3\txiaomin,13\tnihao\t3.1415\t2:4.12,2.34");
-    cout << parser->data().at(0)<<" "<<parser->data().at(1)<<" "<<parser->data().at(2)<<"\n";
-    cout << parserPerson->data().age<<" "<<parserPerson->data().name<<"\n";
-    cout << parserString->data()<<"\n";
-    cout << parserFloat->data()<<"\n";
-    cout << parserFloatVector->data().at(0)<<" "<<parserFloatVector->data().at(1)<<"\n";
-    delete(parser);
+    MyParser<int> *intParser = new IntParser();
+    MyParser<float> *floatParser = new FloatParser();
+    MyParser<string> *stringParser = new StringParser();
+    MyParser<vector<int>> *intArrayParser = new IntArrayParser();
+    MyParser<vector<float>> *floatArrayParser = new FloatArrayParser();
+    MyParser<Person> *personParser = new PersonParser();
+    MyParser<vector<Person>> *personArrayParser = new PersonArrayParser();
+    lp.addParse(intParser);
+    lp.addParse(floatParser);
+    lp.addParse(stringParser);
+    lp.addParse(intArrayParser);
+    lp.addParse(floatArrayParser);
+    lp.addParse(personParser);\
+    lp.addParse(personArrayParser);
+    lp.parse("4\t3.14\ttest\t3:1,2,3\t2:1.3,1.1\tnihao-23\t2:sxm-1,sxm1-2\t2:4.12,2.34");
+    cout << intParser->data();
+    cout << floatParser->data();
+    cout << stringParser->data();
+    cout << intArrayParser->data().at(0);
+    cout << floatArrayParser->data().at(0);
+    cout << personParser->data().name;
+    cout << personArrayParser->data().at(1).name;
+    cout << personArrayParser->data().at(0).age;
+    // cout << intParser->data()<<" "<<parser->data().at(1)<<" "<<parser->data().at(2)<<"\n";
+    // cout << parserPerson->data().age<<" "<<parserPerson->data().name<<"\n";
+    // cout << parserString->data()<<"\n";
+    // cout << parserFloat->data()<<"\n";
+    // cout << parserFloatVector->data().at(0)<<" "<<parserFloatVector->data().at(1)<<"\n";
+
     return 0;
 }
--- parse.h
+++ parse.h
@@ -6,49 +6,191 @@
 #include <stdexcept>
 #include <fstream>
 using namespace std;
+struct Person {
+    std::string name;
+    int age;
+};
 
 // 通用解析类模板
-template <typename T>
-class MyParse {
-    static T parse(std::string str) {
-        T t;
-        return t;
-    };
+class MyParserBase {
+public:
+    virtual int parse(const std::string& str) = 0;
+    virtual ~MyParserBase()=default;
+
 };
 
-// 特化版本，处理 int 类型
-template <>
-class MyParse<int> {
+template<typename  T>
+class MyParser : public MyParserBase {
 public:
-    static int parse(std::string str) {
-        return std::stoi(str);
-    };
-};
+    ~MyParser() override =default;
 
-// 特化版本，处理 float 类型
-template <>
-class MyParse<float> {
-public:
-    static float parse(const std::string& str) {
-        return std::stof(str);
+    virtual T& data() {
+        return _data;
     }
+    private:
+    T _data{};
 };
 
-// 特化版本，处理 string 类型
-template <>
-class MyParse<string> {
+class IntParser final : public MyParser<int> {
 public:
-    static string parse(const std::string& str) {
-        return str;
+    int parse(const std::string& str) override {
+        try {
+            _data = std::stoi(str);
+        } catch (std::exception& e) {
+            return -1;
+        }
+        return 0;
+    };
+    IntParser() {
+        _data = 0;
+    };
+    int& data() override {
+        return _data;
     }
+private:
+    int _data;
 };
 
-// 特化版本，处理 vector 类型
-template <typename T>
-class MyParse<vector<T> > {
-public:
-    static vector<T> parse(const std::string& str) {
-        vector<T> result;
+class FloatParser final : public MyParser<float> {
+public: int parse(const std::string& str) override {
+    try {
+        _data = std::stof(str);
+    } catch (std::exception& e) {
+        return -1;
+    }
+    return 0;
+};
+    FloatParser() {
+        _data = 0.0;
+    };
+    float &data() override {
+        return _data;
+    };
+private:
+    float _data;
+};
+
+class StringParser final : public MyParser<std::string> {
+public: int parse(const std::string& str) override {
+    try {
+        _data = str;
+    } catch (std::exception& e) {
+        return -1;
+    }
+    return 0;
+};
+    std::string &data() override {
+        return _data;
+    };
+private:
+   std::string _data;
+};
+
+
+class PersonParser final : public MyParser<Person> {
+public: int parse(const std::string& str) override {
+    try {
+        std::string::size_type pos = str.find('-');
+        if (pos == std::string::npos) {
+            throw std::invalid_argument("can not find ',' for st");
+        }
+       _data.name =  str.substr(0, pos);
+       _data.age = stoi(str.substr(pos+1, str.size()-pos-1));
+    } catch (std::exception& e) {
+        return -1;
+    }
+    return 0;
+};
+    Person &data() override {
+        return _data;
+    };
+private:
+    Person _data;
+};
+
+
+
+
+class IntArrayParser final : public MyParser<vector<int>> {
+public: int parse(const std::string& str) override {
+    try {
+        vector<int> result;
+        std::string::size_type pos = str.find(':');
+        if (pos == std::string::npos) {
+            throw std::invalid_argument("can not find ':' for vector");
+        }
+        int num = std::stoi(str.substr(0, pos));
+        std::stringstream ss(str.substr(pos + 1, str.size() - pos - 1));
+        std::string sub_str;
+        for (int i=0;i<num;i++) {
+            if (ss.eof()) {
+                throw std::invalid_argument("parse vector");
+            }
+            getline(ss,sub_str,',');
+            result.push_back(stoi(sub_str));
+        }
+        if (!ss.eof()) {
+            throw std::invalid_argument("parse vector");
+        }
+        _data = result;
+    } catch (std::exception& e) {
+        return -1;
+    }
+    return 0;
+};
+    vector<int> &data() override {
+        return _data;
+    };
+    IntArrayParser() {
+        _data = vector<int>{};
+    };
+private:
+    std::vector<int> _data;
+};
+
+
+class FloatArrayParser final : public MyParser<vector<float>> {
+public: int parse(const std::string& str) override {
+    try {
+        vector<float> result;
+        std::string::size_type pos = str.find(':');
+        if (pos == std::string::npos) {
+            throw std::invalid_argument("can not find ':' for vector");
+        }
+        int num = std::stoi(str.substr(0, pos));
+        std::stringstream ss(str.substr(pos + 1, str.size() - pos - 1));
+        std::string sub_str;
+        for (int i=0;i<num;i++) {
+            if (ss.eof()) {
+                throw std::invalid_argument("parse vector");
+            }
+            getline(ss,sub_str,',');
+            result.push_back(stof(sub_str));
+        }
+        if (!ss.eof()) {
+            throw std::invalid_argument("parse vector");
+        }
+        _data = result;
+    } catch (std::exception& e) {
+        return -1;
+    }
+    return 0;
+};
+    vector<float> &data() override {
+        return _data;
+    };
+    FloatArrayParser() {
+        _data = vector<float>{};
+    };
+private:
+    std::vector<float> _data;
+};
+
+
+class PersonArrayParser final : public MyParser<vector<Person>> {
+public: int parse(const std::string& str) override {
+    try {
+        vector<Person> result;
         std::string::size_type pos = str.find(':');
         if (pos == std::string::npos) {
             throw std::invalid_argument("can not find ':' for vector");
@@ -62,68 +204,62 @@ public:
                 throw std::invalid_argument("parse vector");
             }
             getline(ss,sub_str,',');
-            result.push_back(MyParse<T>::parse(sub_str));
+            std::string::size_type pos = sub_str.find('-');
+            if (pos == std::string::npos) {
+                throw std::invalid_argument("can not find ',' for st");
+            }
+
+            Person temp;
+            temp.name =  sub_str.substr(0, pos);
+            temp.age = stoi(sub_str.substr(pos+1, str.size()-pos-1));
+            result.push_back(temp);
         }
         if (!ss.eof()) {
             throw std::invalid_argument("parse vector");
         }
-
-        return result;
+        _data = result;
+    } catch (std::exception& e) {
+        return -1;
     }
+    return 0;
 };
-
-
-class ParserBase {
-public:
-    virtual int parse(const std::string& str) = 0;
-    virtual ~ParserBase() {};
-};
-
-template <typename T>
-class Parser : public ParserBase  {
-public:
-    Parser() {};
-    int parse(const std::string& str) override {
-        try {
-           _data =  MyParse<T>::parse(str);
-        } catch (std::exception& e) {
-            return -1;
-        }
-        return 0;
-    }
-
-    T& data() {
+    vector<Person> &data() override {
         return _data;
-    }
+    };
+    PersonArrayParser() {
+        _data = vector<Person>{};
+    };
 private:
-    T _data;
+    std::vector<Person> _data;
 };
+
+
+
 
 
 class LineParser {
 public:
-    void addParse(ParserBase* p) {
-        parseList.push_back(p);
+    void addParse(MyParserBase* p) {
+        parserList.push_back(p);
     }
-    int parse(std::string str) const {
+    int parse(const std::string& str) const {
         std::stringstream ss(str);
         std::string sub_str;
         unsigned int i = 0;
-        while (getline(ss, sub_str, '\t') && i < parseList.size()) {
-            int res = parseList[i]->parse(sub_str);
-            if (res < 0) {
+        while (getline(ss, sub_str, '\t') && i < parserList.size()) {
+            if (const int res = parserList[i]->parse(sub_str); res < 0) {
                 return -1;
             }
             i++;
         }
-        if (ss.eof() && (i == parseList.size())) {
+        if (ss.eof() && (i == parserList.size())) {
             return 0;
         }
         return -1;
     }
 
 private:
-    vector<ParserBase*> parseList;
+    vector<MyParserBase*> parserList;
 };
 
 
